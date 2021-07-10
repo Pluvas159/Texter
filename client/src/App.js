7d2f5304b139574297af078585607486e15db5da
Pluvas159 <maniksamuel159@gmail.com>
2021-07-10 17:19:55 +0200

    added react, craco and tailwind

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className='w-screen h-screen bg-gray-900 grid place-items-center'>
+      <div className = 'bg-gray-800 w-3/5 h-4/5'></div>
+    </div>
+  );
+}
+
+export default App;
